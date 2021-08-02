7b7086de1f6a602ad0c385f101cac3a5705dfa75
Kevin <kevin.sourlier@gmail.com>
2021-08-02 08:03:36 +0200

    modifications loader

--- frontend/src/components/Loader/LoaderComponent.jsx
+++ frontend/src/components/Loader/LoaderComponent.jsx
@@ -7,59 +7,74 @@ import BluePrint from '../../assets/img/printBlue.png';
 
 class Loader extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {isToggleOn: false};
-    
-        // Cette liaison est nécéssaire afin de permettre
-        // l'utilisation de `this` dans la fonction de rappel.
-        this.handleClick = this.handleClick.bind(this);
-      }
+  constructor(props) {
+    super(props);
+    this.state = { isToggleOn: false };
 
- handleClick() {
-  
-  this.setState({isToggleOn: true});
- 
-    console.log(this.state.isToggleOn);
-
-    
+    // Cette liaison est nécéssaire afin de permettre
+    // l'utilisation de `this` dans la fonction de rappel.
+    this.handleClick = this.handleClick.bind(this);
   }
 
-    render () {
-      const isToggleOn = this.state.isToggleOn;
-      let step;
+  handleClick() {
 
-      if(isToggleOn) {
-        step = <div className="loader-circle-active">
-          <div className="greyPrint-cont">
-            <img src={BluePrint} width="100px"/>
-            </div>
-          <div className="bluePrint-cont">
-            <img src={BluePrint} width="100px"/>
-            </div>
-          
-          <div id="laser"></div>
+    this.setState({ isToggleOn: true });
+
+    console.log(this.state.isToggleOn);
+
+
+  }
+
+  render() {
+    const isToggleOn = this.state.isToggleOn;
+    let step;
+    let text;
+
+    if (isToggleOn) {
+      step = <div className="loader-circle-active">
+        <div className="greyPrint-cont">
+          <img src={BluePrint} width="100px" />
+        </div>
+        <div id="mask"></div>
+        <div className="bluePrint-cont">
+          <img src={BluePrint} height="140px" />
+        </div>
+        <div id="laser"></div>
       </div>
-      } else {
-        step = <div className="loader-circle-inactive">
-          {/*<p className="symbol" onClick={this.handleClick}>PRESS TO UNLOCK DOOR</p>*/}
-          <FontAwesomeIcon icon={faPowerOff} onClick={this.handleClick} size="4x"/>
-          
-          </div>
-      }
-        return (
-        <div className="loader-container">
-                      <div id="loader-top" className="loader">                
-            </div>
-        
-            <div id="loader-bottom" className="loader">                
-            </div>
-            <div className="element">
-                {step}
-            </div>
 
-        </div>)
+      text = "SCANNING...";
+
+    } else {
+      step = <div className="loader-circle-inactive">
+        {/*<p className="symbol" onClick={this.handleClick}>PRESS TO UNLOCK DOOR</p>*/}
+        <FontAwesomeIcon icon={faPowerOff} onClick={this.handleClick} size="4x" />
+      </div>
+      text = "PRESS TO UNLOCK";
     }
+    return (
+      <div className="loader-container">
+        <div id="loader-top" className="loader">
+        </div>
+
+        <div id="loader-bottom" className="loader">
+          <div id="screen-cont">
+            <div id="screen">
+              <div className="top-left cubic"></div>
+              <div className="top-right cubic"></div>
+              <div className="bottom-right cubic"></div>
+              <div className="bottom-left cubic"></div>
+              <div id="text-cont">
+                <p className="screen-text">{text}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="element">
+          {step}
+        </div>
+
+      </div>)
+  }
 }
 
 export default Loader;
